c74da0741c6c5ecbb461f4c1d59055408d8e6769
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-11-14 16:39:23 +0100

    ex11

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
-//ex 9
-//Exercice 9 Conditions :
-//Une boulangerie vend des chocolatines. Pour calculer le prix qu'elle va facturer a ses clients elle a besoin d'un programme. 
-//-Une chocolatine coûte 1€40 a l'unité jusqu'à 10. 
-//-Les 10 suivantes coûtent 1€30 pièce et 
-//-Au-delà elles coûtent 1€20 pièce.
-//Écrire un programme qui calcule le prix à payer  en fonction de la quantité (depuis un prompt) 
-//Afficher dans la console le montant à payer.
+//Exercice 11 Boucles :
+//Nous avons un jeu de dés que nous souhaitons automatiser.
+//Les règles de ce jeu de dès sont les suivantes :
+//-Il y à 2 participants : la banque et le joueur.
+//-Le jeu dure 5 tours.
+//-A chaque tour :
+//    --La banque lance un dès de 6 faces (score 1 à 6).
+//    --Le joueur lance un dès de 6 faces (score 1 à 6).
+//    --Si le joueur fait plus que la banque, ajouter 1 pts au score du joueur,
+//    --Si le joueur fait le même lancé que la banque, ajouter 2 pts au score du joueur.
+//    --Si le joueur fait moins que la banque, ajouter 1 pts au score de la banque.
+//-A la fin des 5 tours le gagnant est celui qui a le score le plus élevé.
+//-> Afficher le gagnant et son score (nbr de pts).
 
-let nombre = parseInt(prompt("Combien de PAINS AU CHOCOLAT ?"));
-let prix;
-if(nombre <= 10){
-    prix = nombre*1.40;
-}else if(nombre<=20){
-    prix = 10*1.4 + ((nombre-10)*1.3);
-}else{
-    prix = 10*1.4 + 10*1.3 + ((nombre-20)*1.2);
+let scoreBanque = 0;
+let scoreJoueur = 0;
+
+for(let i=1; i<=5; i++){
+    let deBanque = Math.floor(Math.random()*6);
+    let deJoueur = Math.floor(Math.random()*6);
+    console.log("Lancer "+i+" : Banque : "+deBanque+" ; Joueur : "+deJoueur);
+    if(deJoueur>deBanque){
+        scoreJoueur ++;
+    }else if(deJoueur == deBanque){
+        scoreJoueur += 2;
+    }else{
+        scoreBanque += 1;
+    }
 }
 
-console.log(prix+"€");
+if(scoreBanque>scoreJoueur){
+    console.log("La banque gagne avec "+scoreBanque+" points.");
+}else if(scoreBanque == scoreJoueur){
+    console.log("Egalité à "+scoreBanque+" points.");
+}else{
+    console.log("Le joueur gagne avec "+scoreJoueur+" points.");
+}
