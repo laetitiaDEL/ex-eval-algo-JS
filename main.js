8736263f44a1e8c0211686c7b18adfde7ec80f46
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-11-15 14:54:57 +0100

    ex13

--- main.js
+++ main.js
@@ -1,67 +1,42 @@
-//Depuis l'exemple de classe Animal :
-//-Instancier 2 nouveaux objets :
-//     -objet chien (chien, 4, 50, 9),
-//     -objet coq(coq, 2, 40, 3),
-//-Modifier la classe Animal :
-//    ->modifier la fonction crier pour intégrer les espèces (chien et coq) et afficher dans la console un cris différent,
-//-Appeler la fonction crier sur les 4 objets :
-//tigre, chat, chien et le coq (pour vérifier si elle fonctionne correctement)
-//-Ajouter une méthode dormir qui va 
-//    -> afficher dans la console (Zzzzzz),
-//-Appeler la méthode dormir sur les objets :
-//tigre, chat, chien et coq.
+//Exercice 13 Classes et Objet :
+//Créer une classe Maison avec comme attributs (constructeur):
+//-Nom,
+//-Longueur,
+//-Largeur,
+//Créer une méthode périmètre de la maison,
+//Créer une méthode surface de la maison,
+//Afficher le périmètre dans la console en mètre linéaire,
+//Afficher la surface dans la console en mètre carré,
 
-class Animal{
+//Bonus :
+//Ajouter un attribut nbrEtage à la maison,
+//Afficher dans la console la surface pour 3 étages en mètre carrés.
+
+class Maison{
     //attributs
-    espece;
-    nbrPattes;
-    age;
-    taille;
-    poid;
-    couleur;
-    //Constructeur
-    constructor(newEspece, newNbrPattes, newTaille, newPoid){
-        this.espece = newEspece;
-        this.nbrPattes = newNbrPattes;
-        this.taille = newTaille;
-        this.poid = newPoid;
+    nom;
+    longueur;
+    largeur;
+    nbrEtages;
+    //constructeur
+    constructor(newNom, newLongueur, newLargeur, newNbrEtages){
+        this.nom = newNom;
+        this.longueur = newLongueur;
+        this.largeur = newLargeur;
+        this.nbrEtages = newNbrEtages;
     }
-    //méthode
-    crier(){
-        if(this.espece == "Tigre" || this.espece == "tigre"){
-            console.log("Grrr");
-        }
-        if(this.espece == "Chat" || this.espece == "chat"){
-            console.log("Miaou");
-        }
-        if(this.espece == "Chien" || this.espece == "chien"){
-            console.log("Waf");
-        }        
-        if(this.espece == "Coq" || this.espece == "coq"){
-            console.log("Cock-a-doodle-do");
-        }
+    //méthodes
+    perimetre(){
+        this.perimetreMaison = (this.largeur+this.longueur)*2;
+        console.log(this.perimetreMaison);
     }
-    dormir(){
-        console.log("Zzzzz");
+    surface(){
+        this.surfaceMaison = (this.largeur*this.longueur*this.nbrEtages);
+        console.log(this.surfaceMaison);
     }
 }
-const tigre = new Animal('tigre', 4, 150, 180);
-const chat = new Animal('chat', 4, 30,4);
 
-console.log(tigre);
-tigre.taille = 180;
-tigre.couleur = "Blanc";
-console.log(tigre);
-tigre.crier();
-chat.crier();
+const maMaison = new Maison('Ma maison', 10, 20, 3);
 
-const chien = new Animal('chien', 4, 50, 9);
-const coq = new Animal('coq', 2, 40, 3);
-
-chien.crier();
-coq.crier();
-
-tigre.dormir();
-chat.dormir();
-chien.dormir();
-coq.dormir();
+maMaison.perimetre();
+maMaison.surface();
