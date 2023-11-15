7baa4d2b51c920a905f05425e90ade1229a9056e
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-11-15 16:45:53 +0100

    ex16

--- main.js
+++ main.js
@@ -1,68 +1,71 @@
-//Exercice 15 Classe et Objet :
-//-Créer une classe Vehicule avec les attributs suivants :
-//nom, couleur, nbrRoue, vitesse,
-//-Instancier 2 nouveaux Vehicule avec les paramètres suivants :
-//-Objet voiture (nomVehicule = « Mercedes CLK », nbrRoue = 4, vitesse 250),
-//-Objet moto (nomVehicule = « Honda CBR », nbrRoue = 2, vitesse = 280),
-//-Créer une fonction detect() qui détecte si le véhicule est une moto ou une voiture (la méthode retourne une string  moto ou voiture avec return),
-//-Exécuter la méthode detect() sur les 2 objets voiture et moto,
-//-Afficher le type de Vehicule (méthode detect afficher dans la console)
-//-Créer une méthode boost qui ajoute 50 à la vitesse d’un Vehicule
-//-Appliquer la méthode boost a la voiture,
-//-Afficher la nouvelle vitesse de la voiture,
+//Exercice 16 classe et Objet :
+//Créer une classe Personnage qui va contenir les éléments suivants :
+//-nom, force, défense, vie
+//-un méthode pour attaquer (elle va soustraire à la vie du personnage la valeur de attaque (attaquant) à défense (notre personnage),
+//Créer une classe Combat qui  va contenir éléments suivants :
+//-nbr de tour, score joueur 1, score joueur 2,
+//-Une méthode qui va lancer les combats (elle va appeler la méthode attaque du joueur 1), (appeler la méthode d'attaque du joueur 2). 
+//Cela va s'exécuter autant de tour que la valeur nbr de tour. 
+//Celui qui arrive à zéro à perdu.
+//Ella va afficher le nom du gagnant.
 
-//Bonus :
-//-Créer une méthode plusRapide() dans la classe Vehicule qui compare la vitesse des différents véhicules (moto et voiture) et retourne le Vehicule le plus rapide des 2 avec un return.
-//-Exécuter la méthode plusRapide(),
-//-Afficher le Vehicule le plus rapide dans la console.
-
-class Vehicule{
+class Personnage{
     nom;
-    couleur;
-    nbrRoues;
-    vitesse;
+    force;
+    defense;
+    vie;
 
-    constructor(newNom, newCouleur, newNbrRoues, newVitesse){
+    constructor(newNom, newForce, newDefense, newVie){
         this.nom = newNom;
-        this.couleur = newCouleur;
-        this.nbrRoues = newNbrRoues;
-        this.vitesse = newVitesse;
+        this.force = newForce;
+        this.defense = newDefense;
+        this.vie = newVie;
     }
 
-    detect(){
-        if(this.nbrRoues == 2){
-            return "Le véhicule "+this.nom+" est une Moto";
-        }
-        if(this.nbrRoues == 4){
-            return "Le véhicule "+this.nom+" est une Voiture";
-        }
+    attack(perso2){
+        perso2.vie -= (this.force-perso2.defense);
     }
 
-    boost(){
-        this.vitesse += 50;
+    display(){
+        console.log(this.nom+" a maintenant "+this.vie+" point(s) de vie.");
+    }
+}
+
+class Combat{
+    nbrTour;
+    J1;
+    J2;
+
+    constructor(newNbrTour, newJ1, newJ2){
+        this.nbrTour = newNbrTour;
+        this.J1 = newJ1;
+        this.J2 = newJ2;
     }
 
-    plusRapide(vehicule){
-        if(this.vitesse>vehicule.vitesse){
-            return "Le véhicule le plus rapide est "+this.nom;
-        }else if(this.vitesse<vehicule.vitesse){
-            return "Le véhicule le plus rapide est "+vehicule.nom;
-        }else{
-            return "Les deux véhicules roulent à la même vitesse."
+    lancer(){
+        for(let i=0; i<this.nbrTour; i++){
+            if(this.J2.vie>this.J1.force-this.J2.defense){
+                this.J1.attack(this.J2);
+                this.J2.display();
+            }else{
+                console.log(this.J2.nom+" n'a plus de PV. "+this.J1.nom+" gagne.");
+                return;
+            }
+            if(this.J1.vie>this.J2.force-this.J1.defense){
+                this.J2.attack(this.J1);
+                this.J1.display();
+            }else{
+                console.log(this.J1.nom+" n'a plus de PV. "+this.J2.nom+" gagne.");
+
+                return;
+            }
         }
     }
 }
 
-//-Objet voiture (nomVehicule = « Mercedes CLK », nbrRoue = 4, vitesse 250),
-const voiture = new Vehicule("Mercedes CLK", "rouge", 4, 250);
-//-Objet moto (nomVehicule = « Honda CBR », nbrRoue = 2, vitesse = 280),
-const moto = new Vehicule("Honda CBR", "noir", 2, 280);
+const sorcier = new Personnage("Grinwald", 37, 4, 48);
+const tank = new Personnage("Grump", 22, 10, 74);
 
-console.log(moto.detect());
-console.log(voiture.detect());
+const combat1 = new Combat(10, sorcier, tank);
 
-voiture.boost();
-
-console.log(voiture.vitesse);
-
-console.log(moto.plusRapide(voiture));
+combat1.lancer();
