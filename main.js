838fcc2e8511401dd56b032cc7425b4e856246c8
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-11-15 09:14:50 +0100

    ex12

--- main.js
+++ main.js
@@ -1,37 +1,14 @@
-//Exercice 11 Boucles :
-//Nous avons un jeu de dés que nous souhaitons automatiser.
-//Les règles de ce jeu de dès sont les suivantes :
-//-Il y à 2 participants : la banque et le joueur.
-//-Le jeu dure 5 tours.
-//-A chaque tour :
-//    --La banque lance un dès de 6 faces (score 1 à 6).
-//    --Le joueur lance un dès de 6 faces (score 1 à 6).
-//    --Si le joueur fait plus que la banque, ajouter 1 pts au score du joueur,
-//    --Si le joueur fait le même lancé que la banque, ajouter 2 pts au score du joueur.
-//    --Si le joueur fait moins que la banque, ajouter 1 pts au score de la banque.
-//-A la fin des 5 tours le gagnant est celui qui a le score le plus élevé.
-//-> Afficher le gagnant et son score (nbr de pts).
+//Exercice 12 Boucles :
+//Ecrire un programme qui demande un nombre de départ (prompt), et qui ensuite affiche la table de multiplication de ce nombre dans la console, présentée comme suit (cas où l'utilisateur entre le nombre 7) :
+//Table de 7 :
+//7 x 1 = 7
+//7 x 2 = 14
+//7 x 3 = 21
+//…
+//7 x 10 = 70
 
-let scoreBanque = 0;
-let scoreJoueur = 0;
-
-for(let i=1; i<=5; i++){
-    let deBanque = Math.floor(Math.random()*6);
-    let deJoueur = Math.floor(Math.random()*6);
-    console.log("Lancer "+i+" : Banque : "+deBanque+" ; Joueur : "+deJoueur);
-    if(deJoueur>deBanque){
-        scoreJoueur ++;
-    }else if(deJoueur == deBanque){
-        scoreJoueur += 2;
-    }else{
-        scoreBanque += 1;
-    }
-}
-
-if(scoreBanque>scoreJoueur){
-    console.log("La banque gagne avec "+scoreBanque+" points.");
-}else if(scoreBanque == scoreJoueur){
-    console.log("Egalité à "+scoreBanque+" points.");
-}else{
-    console.log("Le joueur gagne avec "+scoreJoueur+" points.");
+let nombre = parseInt(prompt("Choisir un nombre :"));
+console.log("Table de "+nombre+" :");
+for(let i=1; i<=10; i++){
+    console.log(nombre+" x "+i+" = "+nombre*i);
 }
