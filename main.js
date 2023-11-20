9536678db62189fe702ae3b16e4c210b645f9bff
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-11-20 16:32:20 +0100

    ex21

--- main.js
+++ main.js
@@ -1,48 +1,38 @@
-//Exercice 20 DOM :
-//Créer une page HTML qui va contenir les éléments suivants :
-//-Un titre h1 -> "Liste des utilisateurs",
-//-Une div,
-//-Dans la div ajouter :
-//        --1 balise img,
-//        --3 paragraphes,
-//Depuis l'objet JS ci-dessous (remplacer les valeurs par vos propres informations):
- const user = {
-  'id' : 1,
-  'nom': 'Deloirs',
-  'prenom': 'Laëtitia',
-  'age': 31,
-  'image' :'./logo.jpg'
+//Exercice 21 DOM :
+//Depuis l'exemple ci-dessus et la correction de l'exercice 9 (prix des chocolatines) :
+//-Dans la page HTML ajouter les éléments suivants :
+//      --un titre h1 (calculer le prix à payer),
+//      --un input de type text ( id = quantite),
+//      --un bouton (onclick = calculer()),
+//      --un paragraphe (id= resultat),
+
+//-Dans la partie JS :
+//créer une fonction qui va se nommer calculer et qui va effectuer le calcul du prix à payer des chocolatines :
+//récupérer la valeur de l'input (id quantité) avec value,
+//afficher dans le paragraphe (id = résultat) le montant à payer.
+//Bonus : Tester si le champ quantité est vide 
+//      => afficher dans le paragraphe "Veuillez choisir un nombre de chocolatine"
+const parag = document.getElementById("resultat");
+
+function calculer(){
+    const quantite = document.getElementById("quantite").value;
+
+    try{
+      if(quantite == ""){
+        throw new Error("Veuillez gnagna");
+      }
+      let prix = 0;
+      if(quantite <= 10){
+        prix = quantite*1.40;
+      }else if(quantite<=20){
+        prix = 10*1.4 + ((quantite-10)*1.3);
+      }else if(quantite >= 21){
+        prix = 10*1.4 + 10*1.3 + ((quantite-20)*1.2);
+      }
+      parag.textContent = prix+"€";
+    }
+    catch(error){
+      parag.textContent = error.message;
+    }
+
 }
-
-//-Ajouter un attribut id à la div qui va avoir en valeur -> (user.id),
-//-Remplacer le contenu des 3 paragraphes par les valeurs user.nom, user.prenom et user.age,
-///-Remplacer l'image par l'url contenu dans user.image
-//Mettre en forme en JS les éléments suivants :
-//-paragraphe nom, prenom, age : police taille 11 px, couleur blue,
-//-taille de l'image : 100 px de large et 100 px de long,
-//-Aligner l'image à en haut à droite et ajouter un margin de 10 px.
-//-remplacer la couleur de fond de la div : rgb(220, 220, 220).
-//NB : pour remplacer le contenu text d'un élément HTML, utilisez textContent
-
-let div = document.querySelector("div");
-div.id = user.id;
-
-let parags = document.querySelectorAll("p");
-parags[0].textContent = user.nom;
-parags[1].textContent = user.prenom;
-parags[2].textContent = user.age;
-
-let img = document.querySelector("img");
-img.src = user.image;
-
-parags.forEach(element => {
-    element.style = "font-size: 11px; color: blue;"
-});
-
-img.style.width = "100px";
-img.style.height = "100px";
-img.style.display = "block";
-img.style.margin = "10px";
-img.style.marginLeft = "auto";
-
-div.style.backgroundColor = "rgb(220, 220, 220)";
