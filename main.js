11584a1419d2b4217ca6384f6d30ba6d5acd5469
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-11-20 12:05:32 +0100

    ex 19

--- main.js
+++ main.js
@@ -1,102 +1,27 @@
-const usersHuman = [{
-        type: "humain",
-        name: "John Doe",
-        email: "j.smith@gmail.com",
-        age: 25
-    },
-    {
-        type: "humain",
-        name: "Jane Smith",
-        email: "ja.doe@sfr.fr",
-        age: 5
-    },
-    {
-        type: "humain",
-        name: "Le Vénérable",
-        email: "levy@gmail.com",
-        age: 500
-    }
-];
+//Exercice 19 DOM :
+//Ajouter les éléments HTML suivants dans votre page index.html :
+//-Titre h1 avec un texte à l'intérieur,
+//-Une div avec du texte à l'intérieur,
+//-Dans la div un paragraphe avec du texte à l'intérieur,
+//En JS :
+//Vous allez changer:
+//-la taille de la police du titre h1 en 14 px,
+//-la police de la div et du paragraphe en Arial,
+//-la couleur du texte du paragraphe en vert,
+//-Ajouter un margin top à la div de 30 px,
+//Remplacer :
+//-le contenu du titre h1 par -> nouveau titre,
+//-le contenu du paragraphe -> nouveau contenu du paragraphe.
+//NB : En utilisant querySelector.
 
-const usersPet = [{
-        type: "animal de compagnie",
-        espece: "chien",
-        name: "Rox",
-        age: 7,
-        propriétaire: "John Doe"
-    },
-    {
-        type: "animal de compagnie",
-        espece: "renard",
-        name: "Roukie",
-        age: 300,
-        propriétaire: "Le Vénérable"
-    }
-];
+titre = document.querySelector("h1");
+div = document.querySelector("div");
+parag = document.querySelector("p");
 
-const usersXeno = [{
-        type: "Xeno",
-        espece: "Krogan",
-        name: "Wrex",
-        menace: "Rouge",
-        age: 45
-    },
-    {
-        type: "Xeno",
-        espece: "Turien",
-        name: "Garrus",
-        menace: "Vert",
-        age: 35
-    },
-    {
-        type: "Xeno",
-        espece: "Asari",
-        name: "Liara",
-        menace: "ULTRA Rouge",
-        age: 25
-    }
-];
+titre.style.fontSize = "14px";
+div.style.fontFamily = "Arial";
+parag.style.color = "green";
+div.style.marginTop = "30px";
 
-const tabData = [];
-tabData.push(usersHuman, usersPet, usersXeno);
-
-function afficherHumain(obj){
-    let affichage = "--------------- \n nom : "+obj.name+"\n email : "+obj.email+"\n age : "+obj.age+"\n---------------";
-    console.log(affichage);
-}
-
-function afficherAnimal(obj){
-    let affichage = "--------------- \n nom : "+obj.name+"\n espece : "+obj.espece+"\n age : "+obj.age+"\n propriétaire : "+obj.propriétaire+"\n---------------";
-    console.log(affichage);
-}
-
-function afficherXeno(obj){
-    let affichage = "--------------- \n nom : "+obj.name+"\n espece : "+obj.espece+"\n age : "+obj.age+"\n niveau de menace : "+obj.menace+"\n---------------";
-    console.log(affichage);
-}
-
-function profil(t){
-    t.forEach(e => {
-        if(e.type == "humain"){
-            afficherHumain(e);
-        }else if(e.type == "animal de compagnie"){
-            afficherAnimal(e);
-        }else if(e.type == "Xeno"){
-            afficherXeno(e);
-        }else{
-            console.log("Type de Profil non Existant");
-        }
-    });
-}
-
-profil(usersHuman);
-profil(usersPet);
-profil(usersXeno);
-
-function profilAll(t){
-    t.forEach(t => {
-        profil(t);
-    });
-}
-
-profilAll(tabData);
+titre.textContent = "nouveau titre";
+parag.textContent = "nouveau contenu du paragraphe"
