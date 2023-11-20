05ee08eb86925d8050e44911fcdef2b5545b547e
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-11-20 16:43:27 +0100

    ex 22

--- main.js
+++ main.js
@@ -1,38 +1,22 @@
-//Exercice 21 DOM :
-//Depuis l'exemple ci-dessus et la correction de l'exercice 9 (prix des chocolatines) :
-//-Dans la page HTML ajouter les éléments suivants :
-//      --un titre h1 (calculer le prix à payer),
-//      --un input de type text ( id = quantite),
-//      --un bouton (onclick = calculer()),
-//      --un paragraphe (id= resultat),
+//Exercice 22 DOM :
+//Transformer l'exercice 6 pour que le programme utilise des éléments HTML plutôt que des prompts et console.log.
+//Partie HTML :
+//Ajouter  les éléments suivants :
+//-un titre h1 -> calculer le prix TTC,
+//-un input de (type text, id = prixHt),
+//-un input de (type text, id = quantite),
+//-un bouton (onclick =calculer()),
+//-un paragraphe (id= resultat),
+//Partie JS :
+//Créer une fonction calculer :
+//-récupérer les valeurs des 2 inputs,
+//-refaire le calcul comme dans l'exercice 8 (correction),
+//-Afficher le résultat dans le paragraphe (id= resultat)
 
-//-Dans la partie JS :
-//créer une fonction qui va se nommer calculer et qui va effectuer le calcul du prix à payer des chocolatines :
-//récupérer la valeur de l'input (id quantité) avec value,
-//afficher dans le paragraphe (id = résultat) le montant à payer.
-//Bonus : Tester si le champ quantité est vide 
-//      => afficher dans le paragraphe "Veuillez choisir un nombre de chocolatine"
-const parag = document.getElementById("resultat");
+let resultat = document.getElementById("resultat");
 
 function calculer(){
-    const quantite = document.getElementById("quantite").value;
-
-    try{
-      if(quantite == ""){
-        throw new Error("Veuillez gnagna");
-      }
-      let prix = 0;
-      if(quantite <= 10){
-        prix = quantite*1.40;
-      }else if(quantite<=20){
-        prix = 10*1.4 + ((quantite-10)*1.3);
-      }else if(quantite >= 21){
-        prix = 10*1.4 + 10*1.3 + ((quantite-20)*1.2);
-      }
-      parag.textContent = prix+"€";
-    }
-    catch(error){
-      parag.textContent = error.message;
-    }
-
+  let prix = document.getElementById("prixHT");
+  let nombre = document.getElementById("quantite");
+  resultat.textContent = prix.value*1.2*nombre.value+"€.";
 }
