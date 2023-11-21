fbfe58c0a9ea1d5ebe81aa4120ce5675d0b04a1c
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-11-21 10:45:43 +0100

    ex23

--- main.js
+++ main.js
@@ -1,22 +1,67 @@
-//Exercice 22 DOM :
-//Transformer l'exercice 6 pour que le programme utilise des éléments HTML plutôt que des prompts et console.log.
+//Exercice 23 DOM :
 //Partie HTML :
-//Ajouter  les éléments suivants :
-//-un titre h1 -> calculer le prix TTC,
-//-un input de (type text, id = prixHt),
-//-un input de (type text, id = quantite),
-//-un bouton (onclick =calculer()),
-//-un paragraphe (id= resultat),
+//Ajouter dans la page les éléments suivants :
+//-1 input type text (id = email),
+//-1 input type password(id= password),
+//-1 input type button (id=bt)
 //Partie JS :
-//Créer une fonction calculer :
-//-récupérer les valeurs des 2 inputs,
-//-refaire le calcul comme dans l'exercice 8 (correction),
-//-Afficher le résultat dans le paragraphe (id= resultat)
+//1 Ajouter un écouteur événement (keyup (touche clavier est relâchée )-> input (id= email) 
 
-let resultat = document.getElementById("resultat");
+//         -> vérifier si le contenu de l'input (id= email) match  le pattern suivant :
+//                   --
 
-function calculer(){
-  let prix = document.getElementById("prixHT");
-  let nombre = document.getElementById("quantite");
-  resultat.textContent = prix.value*1.2*nombre.value+"€.";
-}
+const parag = document.getElementById("error");
+
+
+let regexEmail =/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const btn = document.getElementById("bt");
+
+btn.addEventListener("click", () => {
+  let mail = document.getElementById("email");
+
+  if(regexEmail.test(email.value)){
+    mail.style.backgroundColor = "green";
+  }else{
+    mail.style.backgroundColor = "red";
+  }
+
+  try{
+    if((!regexEmail.test(mail.value)) || (!regexPassword.test(password.value))){
+      throw "Invalide.";
+    }
+    if((regexEmail.test(mail.value)) && (regexPassword.test(password.value))){
+      parag.textContent = "Valide.";
+    }
+  }
+  catch(error){
+    parag.textContent = error;
+  }
+})
+
+//         -> si le email match le pattern mettre l'input (email) en vert (backgroundColor)
+//         -> sinon mettre l'input (email) en rouge(backgroundColor)
+
+//2 Ajouter un écouteur événement (blur(désélection de l'input)-> input (id= password) 
+//         -> vérifier si le contenu de l'input (id= password) match  le pattern suivant :
+let regexPassword = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{12,20}$/;
+let password = document.getElementById("password");
+password.addEventListener("blur", () => {
+  if(regexPassword.test(password.value)){
+    password.style.backgroundColor = "green";
+  }else{
+    password.style.backgroundColor = "red";
+  }
+})
+
+let mail = document.getElementById("email");
+
+
+
+//         -> si le password match le pattern mettre l'input (password) en vert (backgroundColor)
+//         -> sinon mettre l'input (password) en rouge(backgroundColor)
+//Bonus : Ajouter un paragraphe (id = error)  dans la partie HTML :
+//Afficher à l'intérieur :
+//Si le password et l' email sont valides => valide,
+//Si le password ou l' email sont invalides => invalide ,
+//Utiliser test() plutôt que match() dans vos conditions.
