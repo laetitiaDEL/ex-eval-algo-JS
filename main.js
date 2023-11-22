30f235cb0318b43e299751fde01b7791d123c594
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-11-22 15:10:39 +0100

    ex 27

--- main.js
+++ main.js
@@ -1,41 +1,58 @@
-//Depuis la base de code ci-dessus récupérer :
-//température (main),
-//nom de la ville (name),
-//lat et long (coord)
-//Stocker ces valeurs dans des variables.
-//Dans page HTML vous allez ajouter des paragraphes,
-//injecter ces valeurs dans les paragraphes.
-//NB : utiliser les méthodes (createElement, appendChild, textContent).
-//Bonus : convertir la température en degrés celcius 
+let nom = document.getElementById("nom");
+let temps = document.getElementById("temps");
+let temperature = document.getElementById("temperature");
+let ville = "";
 
-
-let url = "https://api.openweathermap.org/data/2.5/weather?lon=1.44&lat=43.6&appid=6f88826d767f323ad19cd282444924e9";
-let ville = document.querySelector("em");
+let newInput = document.createElement("input");
+newInput.setAttribute("id", "newVille");
+let newBtn = document.createElement("button");
+newBtn.textContent = "Entrer";
 let body = document.querySelector("body");
+body.append(newInput, newBtn);
 
-const apiMeteo = fetch(url)
-                    .then(async response=>{
-                        console.log(response);
+//-Ajouter un écouteur d'événement click  sur le bouton qui va :
+let btn = document.querySelector("button");
+btn.addEventListener("click", ()=>{
+  let inputVille = document.getElementById("newVille").value;
+  ville = inputVille;
+  let urlApi = "https://api.openweathermap.org/data/2.5/weather?q="+ville+"&appid=6f88826d767f323ad19cd282444924e9";
+  const apiMeteo = fetch(urlApi)
+                    .then(async response =>{
+                      if(response.status == 404){
+                        let erreur = document.createElement("p");
+                        erreur.textContent = "Cette ville n'existe pas.";
+                        body.appendChild(erreur);
+                      }else{
                         const json = await response.json();
                         console.log(json);
-                        ville.textContent = json.name;
-                        let temp = document.createElement("p");
-                        temp.textContent = (json.main.temp -273.15).toFixed(1)+"°C";
-                        let coord = document.createElement("p");
-                        coord.textContent = JSON.stringify(json.coord);
-                        let weatherImg = document.createElement("img");
-                        weatherImg.setAttribute("src", "https://openweathermap.org/img/wn/"+json.weather[0].icon+"@2x.png");
-                        body.appendChild(weatherImg);
-                        body.appendChild(temp);
-                        body.appendChild(coord);
-                    });
+                        nom.textContent = json.name;
+                        temperature.textContent = (json.main.temp-273.15).toFixed(1)+"°C";
+                        temps.textContent = json.weather[0].main;
+                      }
 
-//Bonus 2 : Afficher à la place de la valeur du temps qu'il fait (weather.main) l'image correspondante (dans une balise image).
-//Le nom de l'image est dans wheater.icon
-//Ressources :
-//Documentation :
-//https://openweathermap.org/weather-conditions
-//icones :
-//https://openweathermap.org/img/wn/10d@2x.png
-//https://openweathermap.org/img/wn/11d@2x.png
-//https://openweathermap.org/img/wn/13d@2x.png
+                    })
+})
+//-Récupérer la valeur de l'input et la passer dans l'url (à la place de ville),
+//-Exécuter le code de partie non Bonus (mais avec la ville saisie dans l'input)
+//-Gérer si le code est 404 (reponse.status == 404) et afficher dans la page la ville n'existe pas. 
+
+//Exercice 27 API et FETCH :
+//Url météo pour récupérer la météo avec le nom d'une ville  :
+//https://api.openweathermap.org/data/2.5/weather?q=ville&appid=6f88826d767f323ad19cd282444924e9
+
+
+//Partie HTML :
+//Créer une page HTML avec les éléments suivants :
+//-Un paragraphe (id=nom),
+//-Un paragraphe (id=temps),
+//-Un paragraphe (id=temperature)
+//Partie JS :
+//-en utilisant le code ci-dessus :
+//-Récupérer les 3 paragraphes HTML,
+//-Passer un nom de ville valide  (à la place de ville dans la variable ville)
+//-Récupérer depuis le json les données suivantes :
+//name(nom de ville),
+//main.temp (météo en degrés Kelvin),
+//weather.main (le temps qu'il fait)
+//Afficher dans la page dans les paragraphes les données correspondantes.
+//Convertir la température en degrés Celcius.
