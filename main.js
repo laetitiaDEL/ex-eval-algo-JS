89e194486006b7b893a9741562044d278d95d4c1
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-11-22 15:59:03 +0100

    ex28

--- main.js
+++ main.js
@@ -1,58 +1,45 @@
-let nom = document.getElementById("nom");
-let temps = document.getElementById("temps");
-let temperature = document.getElementById("temperature");
-let ville = "";
-
-let newInput = document.createElement("input");
-newInput.setAttribute("id", "newVille");
-let newBtn = document.createElement("button");
-newBtn.textContent = "Entrer";
-let body = document.querySelector("body");
-body.append(newInput, newBtn);
-
-//-Ajouter un écouteur d'événement click  sur le bouton qui va :
-let btn = document.querySelector("button");
-btn.addEventListener("click", ()=>{
-  let inputVille = document.getElementById("newVille").value;
-  ville = inputVille;
-  let urlApi = "https://api.openweathermap.org/data/2.5/weather?q="+ville+"&appid=6f88826d767f323ad19cd282444924e9";
-  const apiMeteo = fetch(urlApi)
-                    .then(async response =>{
-                      if(response.status == 404){
-                        let erreur = document.createElement("p");
-                        erreur.textContent = "Cette ville n'existe pas.";
-                        body.appendChild(erreur);
-                      }else{
-                        const json = await response.json();
-                        console.log(json);
-                        nom.textContent = json.name;
-                        temperature.textContent = (json.main.temp-273.15).toFixed(1)+"°C";
-                        temps.textContent = json.weather[0].main;
-                      }
-
-                    })
-})
-//-Récupérer la valeur de l'input et la passer dans l'url (à la place de ville),
-//-Exécuter le code de partie non Bonus (mais avec la ville saisie dans l'input)
-//-Gérer si le code est 404 (reponse.status == 404) et afficher dans la page la ville n'existe pas. 
-
-//Exercice 27 API et FETCH :
-//Url météo pour récupérer la météo avec le nom d'une ville  :
-//https://api.openweathermap.org/data/2.5/weather?q=ville&appid=6f88826d767f323ad19cd282444924e9
-
-
+//Exercice 28 API et fetch :
+//En utilisant l'API :
+//https://adresse.data.gouv.fr/api-doc/adresse
 //Partie HTML :
-//Créer une page HTML avec les éléments suivants :
-//-Un paragraphe (id=nom),
-//-Un paragraphe (id=temps),
-//-Un paragraphe (id=temperature)
+//Ajouter les éléments suivants :
+//-un input text (id=ville_d), (ville de départ)
+//-un input text (id=ville_a), (ville d'arrivée)
+//-un bouton (id=bt),
+//-un paragraphe (id=resultat),
 //Partie JS :
-//-en utilisant le code ci-dessus :
-//-Récupérer les 3 paragraphes HTML,
-//-Passer un nom de ville valide  (à la place de ville dans la variable ville)
-//-Récupérer depuis le json les données suivantes :
-//name(nom de ville),
-//main.temp (météo en degrés Kelvin),
-//weather.main (le temps qu'il fait)
-//Afficher dans la page dans les paragraphes les données correspondantes.
-//Convertir la température en degrés Celcius.
+
+let bt = document.getElementById("bt");
+bt.addEventListener("click", ()=>{
+  let ville_d = document.getElementById("ville_d").value;
+  let ville_a = document.getElementById("ville_a").value;
+  const api = fetch("https://api-adresse.data.gouv.fr/search/?q="+ville_d)
+                .then(async response=>{
+                  let json = await response.json();
+                  console.log(json);
+                })
+});
+//Ajouter un écouteur événement clic sur le bouton qui va éffectuer le traitement suivant :
+//-Récupérer depuis un appel api les coordonnées lat et long de la ville_d,
+//-Récupérer depuis un autre appel api les coordonnées lat et long et la ville_a,
+//-Passer les valeurs dans la fonction ci-dessous (calcule la distance en kilomètre entre 2 jeux de coordonnées :
+function calcul(latDepart, longDepart, latArrive, longArrive){
+    //facteur d'échelle
+    const factE = 1.852;
+    //calcul angle x en deg
+    const xDeg = Math.abs(latDepart-latArrive)*Math.cos((longDepart+longArrive)/2);
+    //calcul angle y en deg
+    const yDeg = Math.abs(longDepart-longArrive);
+    //calcul de la distance en deg
+    const distDeg = Math.sqrt((xDeg**2)+(yDeg**2));
+    //conversion en kilométres
+    const distance = (factE*60*distDeg).toFixed(2);
+    return distance;
+}
+
+//-Afficher la distance dans le paragraphe (id=resultat), sous la forme :
+//la distance est de xxx km
+//Bonus :
+//-Gérer les erreurs : 
+//-si la ville_a ou la ville_d n' existe pas,
+//-afficher dans les inputs ville_a et ou ville_b (placeholder) la ville avec son nom n'existe pas
